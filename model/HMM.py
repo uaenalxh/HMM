0aeafe0e803052350e536b0ac8059c7f8d94ef9a
uaenalxh <uaenalxh@163.com>
2023-05-24 21:12:55 +0800

    Update HMM.py

--- model/HMM.py
+++ model/HMM.py
@@ -1,29 +1,28 @@
 class HMM:
     def __init__(self,o,status,observe,n):
-       self.o=o #观测数据
-       self.status= status #状态集合
-       self.observe=observe# 观测集合
-       self.N = len(self.status)  # 状态集合有多少元素
-       self.M = len(observe)  # 观测集合有多少元素
+       self.o=o 
+       self.status= status 
+       self.observe=observe
+       self.N = len(self.status)  
+       self.M = len(observe)  
        self.A = [[1 / self.N] * self.N] * self.N
        self.B = [[1/self.M]*self.M]* self.N
-       self.T = len(self.o)  # 观测序列一共有多少值
-       self.PI = [1/self.N]*self.N  # 初始状态概率 N个状态 每个状态的概率是1/N
+       self.T = len(self.o)  
+       self.PI = [1/self.N]*self.N  
        self.delte = [[0] * self.N] * self.T
-       self.I = []  # 得到的状态序列是
+       self.I = []  
        self.psi = [[0] * self.N] * self.T
        self.a=self.cal_a()
        self.b=self.cal_b()
        self.n=n
 
     def cal_a(self):
-        #计算前向概率
          o1=self.o[0]
          o1_index=self.observe[o1]
          a=[[0]*self.N]*self.T
          for i in range(self.N):
              a[0][i]=self.PI[i]*self.B[i][o1_index]
-         for t in range(1, self.T):  # 从时刻t=1 开始 到T-1
+         for t in range(1, self.T):  
              ot=self.o[t]
              ot_index = self.observe[ot]
              for i in range(self.N):
@@ -34,7 +33,6 @@ class HMM:
          return a
 
     def cal_b(self):
-        #计算后向概率
         b = [[0] * self.N] * self.T
         for i in range(self.N):
             b[self.T-1][i] = 1
@@ -49,16 +47,12 @@ class HMM:
         return b
 
     def cal_gamma(self, t, i):
-        # 计算李航《统计学习方法》 p202 公式10.24
         sum = 0
         for j in range(self.N):
             sum += self.a[t][j] * self.b[t][j]
-        # print(self.a)
-        # print(self.b)
         return self.a[t][i] * self.b[t][i] / sum
 
     def cal_xi(self, t, i1, j1):
-        # 计算李航《统计学习方法》 p203 公式10.26
         sum = 0
         ot_add_1 = self.o[t + 1]
         ot_ot_add_1_index = self.observe[ot_add_1]
@@ -105,12 +99,11 @@ class HMM:
             self.b = self.cal_b()
 
     def cal_delte(self):
-        # 在书中时刻t的取值是 1到 T 但是写代码数组是从0 开始的 方便起见 我们讲t也从0开始
-        o1=self.o[0]#第一个观测变量是
-        o1_index=self.observe[o1] #第一个观测变量的下标是
+        o1=self.o[0]
+        o1_index=self.observe[o1] 
         for i in range(self.N):
             self.delte[0][i] = self.PI[i]*self.B[i][o1_index]
-        for t in range(1,self.T):#从时刻t=1 开始 到T-1
+        for t in range(1,self.T):
             ot=self.o[t]
             ot_index=self.observe[ot]
             for i in range(self.N):
